367a96b2485fc6c5f101584a054b5b0c74c7e1f4
Hamza Taoussi <tsi.hamza1992@gmail.com>
2018-04-05 11:38:53 +0100

    Creation des Classes typeEau et Eau

---
+++ Facturation/Eau.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facturation
+{
+    public class Eau : Facture
+    {
+        public TypeEau TypeEau { get; set; }
+    }
+}
