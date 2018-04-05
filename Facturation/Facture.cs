7a378ffebe5ba08b91299276ef17cc58321a1882
Hamza Taoussi <tsi.hamza1992@gmail.com>
2018-04-05 10:43:38 +0100

    Creation des Classes Etat et Facture

---
+++ Facturation/Facture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facturation
+{
+    public class Facture
+    {
+        [Key, Column(Order = 1)]
+        public int NPolice { get; set; }
+        [Key, Column(Order = 2)]
+        public string Adresse { get; set; }
+        [Key, Column(Order = 3)]
+        public int NCompteur { get; set; }
+        [Key, Column(Order = 4)]
+        public Etat Etat { get; set; }
+        [Key, Column(Order = 5)]
+        public DateTime Date { get; private set; }
+        [Key, Column(Order = 6)]
+        public string Tel { get; set; }
+    }
+}
