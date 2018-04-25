6a96cfccb4dc6c6f9f1b65604aabf3fc6ed48fd6
Hamza Taoussi <tsi.hamza1992@gmail.com>
2018-04-25 15:25:46 +0100

    some other cleanings

--- Facturation/Migrations/201804251412551_init.cs
+++ Facturation/Migrations/201804251412551_init.cs
@@ -78,41 +78,43 @@ namespace Facturation.Migrations
                 "dbo.RelveeEaux",
                 c => new
                     {
+                        Id = c.Int(nullable: false),
                         Annee = c.DateTime(nullable: false),
-                        trimestre = c.Int(nullable: false),
-                        nIndex = c.Int(nullable: false),
-                        pIndex = c.Int(nullable: false),
-                        nPayer = c.Double(nullable: false),
-                        eau_NPolice = c.Int(),
-                        eau_Adresse = c.String(maxLength: 100),
-                        eau_Date = c.DateTime(),
-                        eau_Tel = c.String(maxLength: 20),
-                        eau_NCompteur = c.Int(),
-                        eau_Reference = c.String(maxLength: 15),
+                        Trimestre = c.Int(nullable: false),
+                        NIndex = c.Int(nullable: false),
+                        PIndex = c.Int(nullable: false),
+                        NPayer = c.Double(nullable: false),
+                        Eau_NPolice = c.Int(),
+                        Eau_Adresse = c.String(maxLength: 100),
+                        Eau_Date = c.DateTime(),
+                        Eau_Tel = c.String(maxLength: 20),
+                        Eau_NCompteur = c.Int(),
+                        Eau_Reference = c.String(maxLength: 15),
                     })
-                .PrimaryKey(t => new { t.Annee, t.trimestre })
-                .ForeignKey("dbo.Eaux", t => new { t.eau_NPolice, t.eau_Adresse, t.eau_Date, t.eau_Tel, t.eau_NCompteur, t.eau_Reference })
-                .Index(t => new { t.eau_NPolice, t.eau_Adresse, t.eau_Date, t.eau_Tel, t.eau_NCompteur, t.eau_Reference });
+                .PrimaryKey(t => new { t.Id, t.Annee, t.Trimestre })
+                .ForeignKey("dbo.Eaux", t => new { t.Eau_NPolice, t.Eau_Adresse, t.Eau_Date, t.Eau_Tel, t.Eau_NCompteur, t.Eau_Reference })
+                .Index(t => new { t.Eau_NPolice, t.Eau_Adresse, t.Eau_Date, t.Eau_Tel, t.Eau_NCompteur, t.Eau_Reference });
             
             CreateTable(
                 "dbo.RelveeElecs",
                 c => new
                     {
+                        Id = c.Int(nullable: false),
                         Annee = c.DateTime(nullable: false),
-                        trimestre = c.Int(nullable: false),
-                        nIndex = c.Int(nullable: false),
-                        pIndex = c.Int(nullable: false),
-                        nPayer = c.Double(nullable: false),
-                        elec_NPolice = c.Int(),
-                        elec_Adresse = c.String(maxLength: 100),
-                        elec_Date = c.DateTime(),
-                        elec_Tel = c.String(maxLength: 20),
-                        elec_NCompteur = c.Int(),
-                        elec_Reference = c.String(maxLength: 15),
+                        Trimestre = c.Int(nullable: false),
+                        NIndex = c.Int(nullable: false),
+                        PIndex = c.Int(nullable: false),
+                        NPayer = c.Double(nullable: false),
+                        Elec_NPolice = c.Int(),
+                        Elec_Adresse = c.String(maxLength: 100),
+                        Elec_Date = c.DateTime(),
+                        Elec_Tel = c.String(maxLength: 20),
+                        Elec_NCompteur = c.Int(),
+                        Elec_Reference = c.String(maxLength: 15),
                     })
-                .PrimaryKey(t => new { t.Annee, t.trimestre })
-                .ForeignKey("dbo.Electricites", t => new { t.elec_NPolice, t.elec_Adresse, t.elec_Date, t.elec_Tel, t.elec_NCompteur, t.elec_Reference })
-                .Index(t => new { t.elec_NPolice, t.elec_Adresse, t.elec_Date, t.elec_Tel, t.elec_NCompteur, t.elec_Reference });
+                .PrimaryKey(t => new { t.Id, t.Annee, t.Trimestre })
+                .ForeignKey("dbo.Electricites", t => new { t.Elec_NPolice, t.Elec_Adresse, t.Elec_Date, t.Elec_Tel, t.Elec_NCompteur, t.Elec_Reference })
+                .Index(t => new { t.Elec_NPolice, t.Elec_Adresse, t.Elec_Date, t.Elec_Tel, t.Elec_NCompteur, t.Elec_Reference });
             
             CreateTable(
                 "dbo.TeleCommunications",
@@ -149,16 +151,16 @@ namespace Facturation.Migrations
         {
             DropForeignKey("dbo.TeleCommunications", "TypeTelecom_id", "dbo.TypeTelecommunications");
             DropForeignKey("dbo.TeleCommunications", "Etat_id", "dbo.Etats");
-            DropForeignKey("dbo.RelveeElecs", new[] { "elec_NPolice", "elec_Adresse", "elec_Date", "elec_Tel", "elec_NCompteur", "elec_Reference" }, "dbo.Electricites");
-            DropForeignKey("dbo.RelveeEaux", new[] { "eau_NPolice", "eau_Adresse", "eau_Date", "eau_Tel", "eau_NCompteur", "eau_Reference" }, "dbo.Eaux");
+            DropForeignKey("dbo.RelveeElecs", new[] { "Elec_NPolice", "Elec_Adresse", "Elec_Date", "Elec_Tel", "Elec_NCompteur", "Elec_Reference" }, "dbo.Electricites");
+            DropForeignKey("dbo.RelveeEaux", new[] { "Eau_NPolice", "Eau_Adresse", "Eau_Date", "Eau_Tel", "Eau_NCompteur", "Eau_Reference" }, "dbo.Eaux");
             DropForeignKey("dbo.Electricites", "TypeElec_id", "dbo.TypeElectricites");
             DropForeignKey("dbo.Electricites", "Etat_id", "dbo.Etats");
             DropForeignKey("dbo.Eaux", "TypeEau_id", "dbo.TypeEaux");
             DropForeignKey("dbo.Eaux", "Etat_id", "dbo.Etats");
             DropIndex("dbo.TeleCommunications", new[] { "TypeTelecom_id" });
             DropIndex("dbo.TeleCommunications", new[] { "Etat_id" });
-            DropIndex("dbo.RelveeElecs", new[] { "elec_NPolice", "elec_Adresse", "elec_Date", "elec_Tel", "elec_NCompteur", "elec_Reference" });
-            DropIndex("dbo.RelveeEaux", new[] { "eau_NPolice", "eau_Adresse", "eau_Date", "eau_Tel", "eau_NCompteur", "eau_Reference" });
+            DropIndex("dbo.RelveeElecs", new[] { "Elec_NPolice", "Elec_Adresse", "Elec_Date", "Elec_Tel", "Elec_NCompteur", "Elec_Reference" });
+            DropIndex("dbo.RelveeEaux", new[] { "Eau_NPolice", "Eau_Adresse", "Eau_Date", "Eau_Tel", "Eau_NCompteur", "Eau_Reference" });
             DropIndex("dbo.Electricites", new[] { "TypeElec_id" });
             DropIndex("dbo.Electricites", new[] { "Etat_id" });
             DropIndex("dbo.Eaux", new[] { "TypeEau_id" });
